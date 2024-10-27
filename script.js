d37289896c352196c5b5beffa6476bdd4e4a2dea
Atif-Mustafa <atif.mustafa0010@gmail.com>
2024-10-27 07:47:11 +0530

    original version

---
+++ script.js
@@ -0,0 +1,9 @@
+let data = [];
+
+
+let fname = document.querySelector('.fname');
+let lname = document.querySelector('.lname');
+let pcountry = document.querySelector('.country');
+let pScore = document.querySelector('.number');
+let button = document.querySelector('button');
+let section2 = document.querySelector('.section2');
